2df747ac2dd9a43915f10699c59a860087858d8c
jsmee <jsmee1@bbox.fr>
2023-01-15 01:39:36 +0100

    Merge branch 'feature/F2' into develop

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,6 +3,8 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 public class AnalyticsCounter {
 
@@ -12,32 +14,40 @@ public class AnalyticsCounter {
 		int rashCount = 0;
 		int pupilCount = 0;
 
+		List<String> symptomList = new ArrayList<>();
+		Map<String, Integer> symptomMap = new HashMap<>();
+
 		// read first symptom
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
-		// count symptoms until end of file
+		// read symptoms until end of file
 		while (line != null) {
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-				System.out.println("number of headaches: " + headacheCount);
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.equals("dialated pupils")) {
-				pupilCount++;
-			}
-
+//            System.out.println("symptom from file: " + line);
+			symptomList.add(line);
 			line = reader.readLine();	// get next symptom
 		}
 
+		// count symptoms
+		for (String symptom : symptomList) {
+			Integer value = symptomMap.get(symptom);
+			if (value == null)
+				symptomMap.put(symptom, 1);
+			else
+				symptomMap.put(symptom, value+1);
+		}
+
+		// Copy the Map into a TreeMap to sort it
+		TreeMap<String, Integer> mySortedMap = new TreeMap<>(symptomMap);
+
 		// write symptom tally
 		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+		mySortedMap.entrySet().forEach(entry -> {
+			try {
+				writer.write(entry.getKey() + " : " + entry.getValue() + "\n");
+			} catch(IOException e) {e.printStackTrace();}
+		});
 		writer.close();
+
 	}
 }
